b58d7ac3cbeb6a7b1d6cbb9eee3dfc555744c87b
ingau <inga.umblija@gmail.com>
2014-09-15 16:45:20 +0100

    Initial commit
    Created Xcode Project GitHubTest2

---
+++ GitHubtest2/GitHubtest2/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GitHubtest2
+//
+//  Created by Inga Umblija on 15/09/2014.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
